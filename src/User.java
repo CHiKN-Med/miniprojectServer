4ee2c4a4287b99438b5bcbc6fb6b9a3f1e7a9dec
lukaslmfdk <56252345+lukaslmfdk@users.noreply.github.com>
2020-10-07 15:12:12 +0200

    User Class - Simple beginning

---
+++ src/User.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class User {
+
+    private int score = 0;
+    private String username;
+
+
+
+    public User (String username){
+
+        this.username = username;
+    }
+
+}
