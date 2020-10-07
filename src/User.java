a2d54d2b6bc3ee1d14251ed0dc11cb810486780b
lukaslmfdk <56252345+lukaslmfdk@users.noreply.github.com>
2020-10-07 15:31:13 +0200

    User Class - Simple beginning

--- src/User.java
+++ src/User.java
@@ -1,15 +1,29 @@
-import java.util.Scanner;
+
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import javax.xml.soap.Text;
 
 public class User {
 
     private int score = 0;
     private String username;
-
-
+    private TextArea ta;
 
     public User (String username){
-
         this.username = username;
     }
 
+    public void showUsername (){
+        ta.appendText(username);
+    }
+
+
 }
+
