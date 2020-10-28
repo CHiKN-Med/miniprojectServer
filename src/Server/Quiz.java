bea3dcea621917634e2b09d65b39c606e7b4e2ba
Francis-KI <56248688+Francis-KI@users.noreply.github.com>
2020-10-28 14:32:37 +0100

    New Questions for quiz

--- src/Server/Quiz.java
+++ src/Server/Quiz.java
@@ -6,9 +6,17 @@ public class Quiz {
 
     String quizDescription;
 
-    String[] questions = {"Hvad hedder Eva?", "Hvad hedder george?", "Er lukas tysker?", "Er chi fra vietnam"};
-    String[] options = {"1)lort \n2)ikke lort\n3)Stor lort", "1)lort \n2)ikke lort", "1) nej\n2) ja", "1) ja\n2) nej"};
-    int[] correctAnswers = {2,1,1,0};
+    String[] questions = {"What is the biggest lake in the world?",
+            "Who wrote the hit 'Ice Ice Baby' from 1989?",
+            "What is the world's biggest land predator?",
+            "What is the highest free fall a human has survived without parachute?",
+            "In what year was the first Harry Potter book published?"};
+    String[] options = {"1) Lake Michigan  \n2) The Caspian Sea \n3) Victoria Lake, \n4)Furesø",
+            "1) Ice Cube \n2) Ice T \n3) Vanilla Ice \n4) Coldplay",
+            "1) Polar Bear \n2) Lion \n3) Bengal Tiger \n4) Pug",
+            "1) 32m \n2) 55m \n3) 93.8m \n4) over 10.000m",
+            "1) 1993 \n2) 1995 \n3) 1998 \n4) 2001"};
+    int[] correctAnswers = {2,3,1,4,3};
     int nrOfQuestions = questions.length;
 
 
@@ -16,8 +24,8 @@ public class Quiz {
     void checkAnswer(UserThread user, Scanner userInput, int i) {
         if(userInput.nextInt() == correctAnswers[i]) {
             user.score++;
-            System.out.println("korrekt"); }
+            System.out.println("Correct"); }
         else {
-            System.out.println("forkert"); }
+            System.out.println("Wrong"); }
     }
 }
