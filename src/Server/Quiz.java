536ea1b864082c61d29237b72a46b1b023dc44df
Niels Henrik Sørensen <56507948+Futjiapples@users.noreply.github.com>
2020-11-04 14:37:22 +0100

    Ekstra spørgsmål

--- src/Server/Quiz.java
+++ src/Server/Quiz.java
@@ -3,17 +3,30 @@ package Server;
 
 
 public class Quiz {
-
+//THE
     String[] questions = {"What is the biggest lake in the world?",
             "Who wrote the hit 'Ice Ice Baby' from 1989?",
             "What is the world's biggest land predator?",
             "What is the highest free fall a human has survived without parachute?",
-            "In what year was the first Harry Potter book published?"};
+            "In what year was the first Harry Potter book published?",
+            "In which city was Anne Frank’s hiding place?",
+            "Which pop group was Fashion designer Victoria Beckham affiliated with?",
+            "The Statue of Liberty was given to the US by which country?",
+            "What country has the most vending machines per capita?",
+            "Who is the highest paid athlete in the world?",
+            "What is the highest-grossing computer animated film?"};
     String[] options = {"1) Lake Michigan  \n\n2) The Caspian Sea \n\n3) Victoria Lake, \n\n4)Furesø",
             "1) Ice Cube \n\n2) Ice T \n\n3) Vanilla Ice \n\n4) Coldplay",
             "1) Polar Bear \n\n2) Lion \n\n3) Bengal Tiger \n\n4) Pug",
             "1) 32m \n\n2) 55m \n\n3) 93.8m \n\n4) over 10.000m",
-            "1) 1993 \n\n2) 1995 \n\n3) 1998 \n\n4) 2001"};
-    int[] correctAnswers = {2,3,1,4,3};
+            "1) 1993 \n\n2) 1995 \n\n3) 1998 \n\n4) 2001",
+            "1) Berlin \n\n2) Frankfurt \n\n3) Amsterdam \n\n4) Auschwitz",
+            "1) Spice Girls \n\n2) Abba \n\n3) Destiny's Child \n\n4) The Pussycat Dolls",
+            "1) Great Britain \n\n2) France \n\n3) Germany \n\n4) Spain",
+            "1) India \n\n2) U.S \n\n3) Japan \n\n4) China",
+            "1) Lebron James \n\n2) Tiger Woods \n\n3) Cristiano Ronaldo \n\n4) Roger Federer",
+            "1) The Lion King (2019) \n\n2) Frozen \n\n3) Toy Story 4 \n\n4) Despicable Me 3"};
+
+    int[] correctAnswers = {2,3,1,4,3,3,1,2,3,4,1};
 
 }
